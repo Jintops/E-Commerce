7627a6aa7dbd5337862e76c4598e4f3bc66a6007
Jintops <jintops2003@gmail.com>
2025-03-06 00:08:02 +0530

    register api

--- Server/server.js
+++ Server/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser=require('cookie-parser')
 const cors=require('cors')
+const authRouter=require('./routes/auth/auth-routes')
 
 const app = express();
 
@@ -22,6 +23,8 @@ app.use(cors({
 
 app.use(cookieParser());
 app.use(express.json());
+app.use('/api/auth',authRouter)
+
 const Port = process.env.PORT || 5000;
 
 mongoose.connect('mongodb+srv://jintops2003:jinto%402003@cluster0.jpkxj.mongodb.net/')
