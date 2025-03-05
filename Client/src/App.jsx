93e4b725d14117f84e6b410db6f4e3122132d661
Jintops <jintops2003@gmail.com>
2025-03-05 14:49:24 +0530

    login & register

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -15,25 +15,38 @@ import Account from './pages/shopping-view/Account'
 import Checkout from './pages/shopping-view/Checkout'
 import Home from './pages/shopping-view/Home'
 import Listing from './pages/shopping-view/Listing'
+import CheckAuth from './components/common/CheckAuth'
+import UnauthPage from './pages/unauth-page/UnauthPage'
 
 const App = () => {
+  const isAuthenticated=false;
+  const user=null;
+
+
   return (
     <div>
      
       <Routes>
-        <Route path='/auth' element={<Layouts/>}>
+        <Route path='/auth' element={
+          <CheckAuth isAuthenticated={isAuthenticated} user={user}> <Layouts/>
+          </CheckAuth>
+        }>
           <Route path='login' element={<Login />} />
           <Route path='register' element={<Register />} />
         </Route>
       
-          <Route path='/admin' element={<Layout/>}>
+          <Route path='/admin' element={
+            <CheckAuth isAuthenticated={isAuthenticated} user={user}><Layout/></CheckAuth>
+          }>
           <Route path='dashboard' element={<Dashboard/>}></Route>
           <Route path='features' element={<Features/>}></Route>
           <Route path='products' element={<Products/>}></Route>
           <Route path='orders' element={<Orders/>}></Route>
           </Route>
           
-          <Route path='/shop' element={<ShoppingLayout/>}> 
+          <Route path='/shop' element={
+            <CheckAuth isAuthenticated={isAuthenticated} user={user}><ShoppingLayout/></CheckAuth>
+          }> 
           <Route path='account' element={<Account/>}></Route>
           <Route path='checkout' element={<Checkout/>}></Route>
           <Route path='home' element={<Home/>}></Route>
@@ -41,6 +54,7 @@ const App = () => {
           </Route>
 
           <Route path='*' element={<NotFound/>}></Route>
+          <Route path='/unauth-page' element={<UnauthPage/>}></Route>
       </Routes>
     </div>
   )
